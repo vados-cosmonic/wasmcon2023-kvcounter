81df03f29413bc8be5161e602cdd18d7e8004f57
Victor Adossi <vadossi@cosmonic.com>
2023-08-17 06:45:35 +0900

    wip: add some implementation using keyvalue
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,40 @@ use wasi::{
         ResponseOutparam,
     },
     io::streams::write,
+    keyvalue::{
+        readwrite::{get, set},
+        types::{
+            incoming_value_consume_sync, new_outgoing_value, open_bucket, outgoing_value_write_body,
+        },
+    },
 };
 
 use crate::exports::wasi::http::incoming_handler::{IncomingHandler, IncomingRequest};
+
+const BUCKET: &str = "default";
 
 struct KvCounter;
 
 impl KvCounter {
     /// Increment (possibly negatively) the counter for a given key
-    fn increment_counter(_key: impl AsRef<str>, _amount: i32) {
-        todo!()
+    fn increment_counter(bucket: u32, key: &String, amount: i32) {
+        let incoming_value = get(bucket, key).expect("failed to get resource");
+        // TODO: set the value to 1 if key is missing
+
+        let bytes =
+            incoming_value_consume_sync(incoming_value).expect("failed to parse incoming bytes");
+        let value: i32 = String::from_utf8(bytes)
+            .expect("failed to parse string from returned bytes")
+            .trim()
+            .parse()
+            .expect("failed to parse numeric value from bucket");
+
+        let outgoing_value = new_outgoing_value();
+        let stream =
+            outgoing_value_write_body(outgoing_value).expect("failed to write outgoing value");
+        write(stream, (value + amount).to_string().as_bytes())
+            .expect("failed to write to outgoing value stream");
+        set(bucket, key, outgoing_value).expect("failed to set value");
     }
 
     /// Retrieve static asset from disk
@@ -29,10 +53,12 @@ impl KvCounter {
 fn write_wasi_http_response(body: impl AsRef<[u8]>, response_outparam: ResponseOutparam) {
     let headers = new_fields(&Vec::new());
     let outgoing_response = new_outgoing_response(200, headers).expect("failed to create response");
-    let outgoing_stream = outgoing_response_write(outgoing_response).expect("failed to write outgoing response");
+    let outgoing_stream =
+        outgoing_response_write(outgoing_response).expect("failed to write outgoing response");
     write(outgoing_stream, body.as_ref()).expect("failed to write output to stream");
     finish_outgoing_stream(outgoing_stream);
-    set_response_outparam(response_outparam, Ok(outgoing_response)).expect("failed to set response");
+    set_response_outparam(response_outparam, Ok(outgoing_response))
+        .expect("failed to set response");
 }
 
 impl IncomingHandler for KvCounter {
@@ -51,7 +77,8 @@ impl IncomingHandler for KvCounter {
         // Generate an outgoing request
         match (method, trimmed_path.as_slice()) {
             (Method::Get, ["api", "counter"]) => {
-                KvCounter::increment_counter("default", 1);
+                let bucket = open_bucket(BUCKET).expect("failed to open bucket");
+                KvCounter::increment_counter(bucket, &String::from("default"), 1);
 
                 // TODO: actually perform counter update
 
@@ -59,7 +86,8 @@ impl IncomingHandler for KvCounter {
                 write_wasi_http_response("0", response);
             }
             (Method::Get, ["api", "counter", counter]) => {
-                KvCounter::increment_counter(counter, 1);
+                let bucket = open_bucket(BUCKET).expect("failed to open bucket");
+                KvCounter::increment_counter(bucket, &counter.to_string(), 1);
 
                 // TODO: actually perform counter update
 
@@ -73,7 +101,8 @@ impl IncomingHandler for KvCounter {
 
                 // TODO: write the body of the asset to disk
 
-                write_wasi_http_response(r#"
+                write_wasi_http_response(
+                    r#"
 <!DOCTYPE html>
 <html>
   <head><title>Example Page</title></head>
@@ -81,11 +110,14 @@ impl IncomingHandler for KvCounter {
   <h1>Not Implemented</h1>
   </body>
 </html>
-"#, response);
+"#,
+                    response,
+                );
             }
             (_, _) => {
                 // TODO: return not found
-                write_wasi_http_response(r#"
+                write_wasi_http_response(
+                    r#"
 <!DOCTYPE html>
 <html>
   <head><title>Not Found</title></head>
@@ -93,7 +125,9 @@ impl IncomingHandler for KvCounter {
   <h1>404 - Page not found</h1>
   </body>
 </html>
-"#, response);
+"#,
+                    response,
+                );
             }
         };
 
